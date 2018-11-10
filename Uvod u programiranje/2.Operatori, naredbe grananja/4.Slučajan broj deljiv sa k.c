c748b4011477f65979f33812697cdf246196fbe4
JPaja <jovan.pavlovic99@gmail.com>
2018-11-10 19:41:37 +0100

    Popravka: Slučajan broj deljiv sa k
    Obrisane nepotrebne linije

--- Uvod u programiranje/2.Operatori, naredbe grananja/4.Slučajan broj deljiv sa k.c
+++ Uvod u programiranje/2.Operatori, naredbe grananja/4.Slučajan broj deljiv sa k.c
@@ -1,3 +1,5 @@
+//http://petlja.org/biblioteka/r/problemi/uvod-u-programiranje/04_slucajan_broj_deljiv_sa_k
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,7 +11,5 @@ int main()
     srand(time(0));
     n = (n + k - 1) - ~(k - 1); //dodaje kolko fali da n bude deljivo sa k
     m -= m % k;
-    // (n + k - 1) - ~(k - 1) + (rand() % ((m - n) / k)) * k
     printf("%d", n + (rand() % (m / n + 1)) * k);
-    system("pause");
 }
